f30293e273825e0b2a54526dcc24724911ffef8a
Darwin Smith <darwin911@gmail.com>
2019-11-19 00:18:49 -0500

    changes entry to be Table

--- src/components/Entry.tsx
+++ src/components/Entry.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import moment from 'moment';
+import { Table, Label } from 'semantic-ui-react';
 
 interface Props {
   entry: DatabaseEntry;
@@ -8,22 +9,35 @@ interface Props {
 interface DatabaseEntry {
   id: string;
   action: string;
-  timestamp: firebase.firestore.Timestamp;
+  timestamp: number;
   day: number;
   month: number;
   year: number;
 }
 
 export const Entry: React.FC<Props> = ({ entry }) => {
-  const time = moment(entry.timestamp.seconds * 1000).format('hh:mm:ss a');
-
+  const time = moment(entry.timestamp).format('hh:mm:ss a');
   return (
-    <div key={entry.id} className='entry'>
-      <p>Action: {entry.action}</p>
-      <p className='entry__date'>
-        Date: {entry.month + 1}/{entry.day}/{entry.year}
-      </p>
-      <p className='entry__time'>Time: {time}</p>
-    </div>
+    <Table className='entry' celled collapsing>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>Time</Table.HeaderCell>
+          <Table.HeaderCell>Action</Table.HeaderCell>
+          <Table.HeaderCell>Date</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+
+      <Table.Body>
+        <Table.Row>
+          <Table.Cell>
+            <Label ribbon>{time}</Label>
+          </Table.Cell>
+          <Table.Cell>{entry.action}</Table.Cell>
+          <Table.Cell>
+            {entry.month}/{entry.day}/{entry.year}
+          </Table.Cell>
+        </Table.Row>
+      </Table.Body>
+    </Table>
   );
 };
