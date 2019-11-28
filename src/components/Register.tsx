457ea980f5e18d5aa759e93e40fefaf6b562f799
Darwin Smith <darwin911@gmail.com>
2019-11-28 17:22:50 -0500

    WIP register form created [no function]

---
+++ src/components/Register.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { Form, Message } from 'semantic-ui-react';
+import firebase from '../firebase';
+
+interface Props {
+  setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
+  toggleLoginForm: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+interface ErrorCode {
+  code: string;
+  message: string;
+}
+
+export const Register: React.FC<Props> = ({
+  setIsLoggedIn,
+  toggleLoginForm
+}) => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<ErrorCode | null>(null);
+
+  const handleRegister = async () => {
+    setIsLoading(true);
+    console.log(email, password);
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(thing => {
+        console.log(thing);
+        setIsLoading(false);
+      })
+      .catch(err => {
+        setError(err);
+        console.log(err);
+        setIsLoading(false);
+      });
+  };
+
+  return (
+    <Form loading={isLoading} error>
+      <Form.Input
+        type='Email'
+        placeholder='email'
+        onChange={e => setEmail(e.target.value)}
+      />
+      <Form.Input
+        type='password'
+        placeholder='Password'
+        onChange={e => setPassword(e.target.value)}
+      />
+      <Form.Button onClick={handleRegister}>Create Account</Form.Button>
+      <Form.Button onClick={() => toggleLoginForm(val => !val)}>
+        Log In
+      </Form.Button>
+      {error && <Message error header={error.code} content={error.message} />}
+    </Form>
+  );
+};
