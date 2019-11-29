1367367d523106cc9c400afe9e68a0238654cf9d
Darwin Smith <darwin911@gmail.com>
2019-11-29 12:23:47 -0500

    filters entries for currentUser only

--- src/components/TrackerContainer.tsx
+++ src/components/TrackerContainer.tsx
@@ -17,6 +17,7 @@ interface Props {
 }
 
 interface NewEntry {
+  userId: string | undefined;
   action: string;
   timestamp: number;
   day: number;
@@ -43,20 +44,23 @@ const useEntries = () => {
   const [todaysEntries, setTodaysEntries] = useState();
 
   useEffect(() => {
-    const unsubscribe = firebase
-      .firestore()
-      .collection('entries')
-      .where('dayOfYear', '==', today)
-      .onSnapshot(snapshot => {
-        const newEntries = snapshot.docs.map(doc => ({
-          id: doc.id,
-          ...doc.data()
-        }));
+    const user = firebase.auth().currentUser;
+    if (user) {
+      const unsubscribe = firebase
+        .firestore()
+        .collection('entries')
+        .where('dayOfYear', '==', today)
+        .where('userId', '==', user.uid)
+        .onSnapshot(snapshot => {
+          const newEntries = snapshot.docs.map(doc => ({
+            id: doc.id,
+            ...doc.data()
+          }));
 
-        setTodaysEntries(newEntries);
-      });
-
-    return () => unsubscribe();
+          setTodaysEntries(newEntries);
+        });
+      return () => unsubscribe();
+    }
   }, []);
 
   return todaysEntries;
@@ -71,23 +75,27 @@ export const TrackerContainer: React.FC<Props> = ({
   console.log(typeof entries, entries);
 
   const addEntry = (action: string) => {
-    const date = new Date();
-    const entry: NewEntry = {
-      action,
-      timestamp: Date.now(),
-      day: date.getDate(),
-      month: date.getMonth() + 1,
-      year: date.getFullYear(),
-      dayOfYear: today
-    };
+    if (user) {
+      const { uid } = user;
+      const date = new Date();
+      const entry: NewEntry = {
+        userId: uid,
+        action,
+        timestamp: Date.now(),
+        day: date.getDate(),
+        month: date.getMonth() + 1,
+        year: date.getFullYear(),
+        dayOfYear: today
+      };
 
-    console.log(entry);
+      console.log(entry);
 
-    firebase
-      .firestore()
-      .collection('entries')
-      .add(entry)
-      .catch(err => console.log(err));
+      firebase
+        .firestore()
+        .collection('entries')
+        .add(entry)
+        .catch(err => console.log(err));
+    }
   };
 
   const handleLogout = () => {
@@ -107,7 +115,7 @@ export const TrackerContainer: React.FC<Props> = ({
   };
 
   return (
-    <div style={{ maxWidth: 600, margin: '0 auto' }}>
+    <div style={{ width: 600, maxWidth: '90vw', margin: '0 auto' }}>
       <Button onClick={handleLogout}>Logout</Button>
       <h1>Hello {user && user.name}!</h1>
       <h2>Today's entry: </h2>
