b557c2cfed42b5da66e9448fc171545133a85e6c
Darwin Smith <darwin911@gmail.com>
2019-11-29 10:26:08 -0500

    Checks for currentUser on load

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './scss/style.css';
 import 'semantic-ui-css/semantic.min.css';
+import firebase from './firebase';
 import { Authentication } from './components/Authentication';
-
 import { TrackerContainer } from './components/TrackerContainer';
+
+interface UserObject {
+  name: string | null | undefined;
+  email: string | null | undefined;
+  uid: string | undefined;
+}
 
 const App: React.FC = () => {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [currentUser, setCurrentUser] = useState<UserObject | null>(null);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        // User is signed in.
+        const userObj = {
+          name: user.displayName,
+          email: user.email,
+          uid: user.uid
+        };
+        console.log('User is signed in');
+        console.log(userObj);
+        setCurrentUser(userObj);
+        setIsLoggedIn(true);
+      } else {
+        // No user is signed in.
+        console.log('not signed in');
+      }
+    });
+  }, []);
+
   return (
     <div className='App'>
-      {isLoggedIn ? (
-        <TrackerContainer />
+      {isLoggedIn && currentUser ? (
+        <TrackerContainer
+          user={currentUser}
+          setIsLoggedIn={setIsLoggedIn}
+          setCurrentUser={setCurrentUser}
+        />
       ) : (
         <>
           <Authentication setIsLoggedIn={setIsLoggedIn} />
